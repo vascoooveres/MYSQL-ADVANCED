57af838182224e0882331a694f766329eedcb33c
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2022-02-03 15:16:15 +0100

    antwoorden sql opdracht 2

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats)
+VALUE ('Sjak', 'Jakken', '2002-11-11', 'Uithoorn')
 
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats)
+VALUE ('Frank', 'Franksen', '2002-02-02', 'Haarlem')
+
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats)
+VALUE ('Harry', 'Jazz', '2001-11-11', 'Amstelveen')
