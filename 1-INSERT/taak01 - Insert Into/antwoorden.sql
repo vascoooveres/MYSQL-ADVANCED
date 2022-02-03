17556a8a31a1a0b1cee76da4a09adfa21676b092
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2022-02-03 14:57:05 +0100

    antwoorden sql opdracht 1

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUE (5, 'Vasco', 'Overes', '9B', 'Hendrik Roeters 3', '1356FG', '2005-08-11', 'Amsterdam')
 
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUE (6, 'Pieter', 'Pietersen', '9A', 'Jan Borten straat 23', '2399DA', '2001-03-23', 'Aalsmeer')
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUE (7, 'Ron', 'Beton', '9C', 'Burgemeester Kootlaan 3', '1411KD', '2002-12-30', 'Uithoorn')
