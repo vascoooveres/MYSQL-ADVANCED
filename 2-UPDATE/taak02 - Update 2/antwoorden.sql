b29c7fede0761673018f603a0a1e433f6785b3aa
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2022-02-03 22:04:58 +0100

    opdrachten 2 sql

--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,8 +1,16 @@
 -- Opdracht 1
-
+UPDATE studenten2
+SET klas = '9X'
+WHERE klas = '9A';
 -- Opdracht 2 
-
+UPDATE studenten2
+SET klas = '9Z'
+WHERE student_id > 7;
 -- Opdracht 3
-
+UPDATE studenten2
+SET woonplaats = 'Amsterdam'
+WHERE student_id <= 2 OR student_id >7;
 -- Opdracht 4
-
+UPDATE studenten2
+SET woonplaats = 'Uithoorn'
+WHERE klas = '9X' AND woonplaats = 'Amsterdam';
